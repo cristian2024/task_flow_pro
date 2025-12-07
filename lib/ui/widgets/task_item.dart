2c347222318c4ce8164faf2ad1ce0ca43ac37612
Cristian Daniel Ramirez Marin <cristiandramirezm.2024@gmail.com>
2025-12-07 09:10:08 -0500

    feature: initial ui implementation for task management

---
+++ lib/ui/widgets/task_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../../domain/model/task.dart';
+
+class TaskItem extends StatelessWidget {
+  const TaskItem(
+    this.task, {
+    super.key,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
